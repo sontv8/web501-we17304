7809e53cd6991a2ecf32ffa5eb4f77e93784d4eb
sontv8 <sontv8@fpt.edu.vn>
2023-02-23 15:54:21 +0700

    validate

--- pages/admin/project-add.js
+++ pages/admin/project-add.js
@@ -1,36 +1,51 @@
 // import { projectList } from "../../data";
 import { addProject } from "../../api/project";
 import { router, useEffect } from "../../libs";
+import { object, string } from 'yup';
 
+const projectSchema = object({
+    name: string().required(),
+    desc: string().required()
+})
 const AdminProjectAddPage = () => {
     // kiểm tra localStorage có dữ liệu không?
     // nếu có thì lấy dữ liệu
     // ngược lại thì gán mảng rỗng
-    
-    const projectList = JSON.parse(localStorage.getItem("projects"))||[];
+
+    const projectList = JSON.parse(localStorage.getItem("projects")) || [];
     console.log(projectList);
 
     useEffect(() => {
         const form = document.getElementById("form-add");
         const projectName = document.getElementById("project-name");
+        const projectDesc = document.getElementById("project-desc");
 
         form.addEventListener("submit", (e) => {
             e.preventDefault();
             // tạo ra 1 object mới lấy dữ liệu từ form
             const newProject = {
                 // id: projectList.length + 1,
-                name: projectName.value
+                name: projectName.value,
+                desc: projectDesc.value
             };
-            // fetch("http://localhost:3000/projects",{
-            //     // GET, POST, PUT, DELETE...
-            //     method:"POST",
-            //     headers:{
-            //         'Content-Type':'application/json'
-            //     },
-            //     body: JSON.stringify(newProject)
-            // }).then(()=> router.navigate("/admin/projects"))
 
-            addProject(newProject).then(()=> router.navigate("/admin/projects"))
+            projectSchema.validate(newProject, { abortEarly: false })
+                .then(() => {
+                    fetch("http://localhost:3000/projects", {
+                        // GET, POST, PUT, DELETE...
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(newProject)
+                    }).then(() => router.navigate("/admin/projects"))
+                })
+                .catch(error => {
+                    error.inner.forEach(error => console.log(error.path))
+                })
+            //
+
+            addProject(newProject).then(() => router.navigate("/admin/projects"))
 
 
             // thêm vào mảng projectList
@@ -50,6 +65,10 @@ const AdminProjectAddPage = () => {
             <div class="form-group mb-3">
                 <label for="" class="form-label">Tên dự án</label>
                 <input type="text" class="form-control" id="project-name" />
+            </div>
+            <div class="form-group mb-3">
+                <label for="" class="form-label">Mô tả dự án</label>
+                <input type="text" class="form-control" id="project-desc" />
             </div>
             <div class="form-group">
                 <button class="btn btn-primary">Thêm dự án</button>
